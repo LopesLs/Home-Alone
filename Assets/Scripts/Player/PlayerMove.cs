83d145ce9827b41cb655787f468083968acc66cb
Carlos Lopez ✔ <orlandocarlosjose@gmail.com>
2023-01-12 08:02:40 -0300

    Initial Commit

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName = "Horizontal";
+    [SerializeField] private string verticalInputName = "Vertical";
+
+    [SerializeField] private float movementSpeed = 1.5f;
+
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        
+        // Implementação de sprint dentro do jogo
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            movementSpeed = 3f;
+        }
+        
+        else {
+            movementSpeed = 1.5f;
+        }
+
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;     // CharacterController.SimpleMove() applies deltaTime
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        //simple move applies delta time automatically
+        charController.SimpleMove(forwardMovement + rightMovement);
+    }
+}

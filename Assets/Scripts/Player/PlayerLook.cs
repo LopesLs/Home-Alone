5645c0b5fba65756d2ef0ef0b08cfe81a55c69bf
Carlos Lopez ✔ <orlandocarlosjose@gmail.com>
2023-01-15 09:16:59 -0300

    (docs) adding comments

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -1,35 +1,48 @@
+// This script is used to configure the camera of player
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+    // Input axes for mouse movement
     [SerializeField] private string mouseXInputName = "Mouse X";
     [SerializeField] private string mouseYInputName = "Mouse Y";
+    
+    // Sensitivity of the mouse movement
     [SerializeField] private float mouseSensitivity = 150f;
 
+    // Reference to the player's body transform
     [SerializeField] private Transform playerBody;
+    
+    // Variable to store the clamp value for the X axis rotation
     private float xAxisClamp;
+    
+    // Flag to check if the cursor is locked
     private bool m_cursorIsLocked = true;
 
     private void Awake()
     {
+        // Lock the cursor on start
         LockCursor();
         xAxisClamp = 0.0f;
     }
 
     private void LockCursor()
     {
-       
-        if (Input.GetKeyUp(KeyCode.Escape))
+        // Check if the F1 key is released and lock the cursor
+        if (Input.GetKeyUp(KeyCode.F1))
         {
             m_cursorIsLocked = false;
         }
+        // Check if the left mouse button is released and unlock the cursor
         else if (Input.GetMouseButtonUp(0))
         {
             m_cursorIsLocked = true;
         }
 
+        // Set the cursor lock state and visibility based on the flag
         if (m_cursorIsLocked)
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -40,21 +53,24 @@ public class PlayerLook : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
-        
     }
 
     private void Update()
     {
+        // Call the camera rotation function
         CameraRotation();
     }
 
     private void CameraRotation()
     {
+        // Get input for horizontal and vertical mouse movement
         float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
+        // Keep track of the current rotation on the x-axis
         xAxisClamp += mouseY;
 
+        // Limit the rotation on the x-axis to prevent the camera from flipping
         if (xAxisClamp > 90.0f)
         {
             xAxisClamp = 90.0f;
@@ -68,14 +84,22 @@ public class PlayerLook : MonoBehaviour
             ClampXAxisRotationToValue(90.0f);
         }
 
+        // Rotate the camera on the y-axis
         transform.Rotate(Vector3.left * mouseY);
+        
+        // Rotate the player body on the x-axis
         playerBody.Rotate(Vector3.up * mouseX);
     }
 
     private void ClampXAxisRotationToValue(float value)
     {
+        // Get the current rotation values
         Vector3 eulerRotation = transform.eulerAngles;
+        
+        // Set the x-axis rotation to the specified value
         eulerRotation.x = value;
+        
+        // Apply the new rotation values
         transform.eulerAngles = eulerRotation;
     }
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,46 +1,59 @@
+// This script is used for player movement in the game
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
-{
+{   
+    // Variables for horizontal and vertical input axis names
     [SerializeField] private string horizontalInputName = "Horizontal";
     [SerializeField] private string verticalInputName = "Vertical";
 
+    // Variable for movement speed
     [SerializeField] private float movementSpeed = 1.5f;
-
+    
+    // Reference to character controller component
     private CharacterController charController;
 
+    // Get reference to character controller component on awake
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
     }
 
+    // Update player movement in each frame
     private void Update()
     {
         PlayerMovement();
     }
 
+    // Function for player movement
     private void PlayerMovement()
     {
         
-        // Implementação de sprint dentro do jogo
+        // Check if left shift key is pressed for sprinting
         if (Input.GetKey(KeyCode.LeftShift)) {
+            
+            // Increase movement speed
             movementSpeed = 3f;
         }
         
+        // If left shift is not pressed, set movement speed back to normal        
         else {
             movementSpeed = 1.5f;
         }
 
-        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;     // CharacterController.SimpleMove() applies deltaTime
+        // Get input for vertical and horizontal movement
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
         float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
 
+        // Create vectors for forward and right movement based on vertical and horizontal inputs
         Vector3 forwardMovement = transform.forward * vertInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        //simple move applies delta time automatically
+        // Move the character controller based on forward and right movement vectors
         charController.SimpleMove(forwardMovement + rightMovement);
     }
 }
